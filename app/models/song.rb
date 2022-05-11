ba352624766700b6a19a5220e496925ca0486e50
Joe <zvmboni@gmail.com>
2022-05-11 16:29:08 -0700

    commit tree 0641aa1d7885e09e26285fd8b669e028b82ac5be

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,9 @@
-class Song < ActiveRecord::Base
+lclass Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
   def get_genre_name
+    self.genre.name
   end
 
   def drake_made_this
